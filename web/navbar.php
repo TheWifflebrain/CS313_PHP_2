8873042c4f8c0304c5a965eb4caed6ef9120974d
TheWifflebrain <blackice150@gmail.com>
2019-05-04 21:01:36 -0600

    php nav bar pt3

--- web/navbar.php
+++ web/navbar.php
@@ -5,11 +5,11 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 	<ul class="navbar-nav">
 		<li class="nav-item <?php if ($file === 'homepage') echo 'active' ?>">
-			<a href="homepage.php">About Us</a>
+			<a href="homepage.php">Smash Ultimate Homepage</a>
 		</li>
 
 		<li class="nav-item <?php if ($file === 'assignments') echo 'active' ?>">
-			<a href="assignments.php">Home</a>
+			<a href="assignments.php">Assignments Page</a>
 		</li>
 	<ul>
 </nav>
