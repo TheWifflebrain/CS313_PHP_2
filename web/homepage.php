2df11aca2a9acab252668ba1629fa5f9f2cb8aea
TheWifflebrain <blackice150@gmail.com>
2019-05-04 21:15:03 -0600

    jumbotron styling4

--- web/homepage.php
+++ web/homepage.php
@@ -24,7 +24,7 @@
     <div>
         <h2>About</h2>
         <p>
-            Super Smash Bros. Ultimate[b] is a 2018 crossover fighting game developed by Bandai Namco Studios and Sora Ltd., 
+            Super Smash Bros. Ultimate is a 2018 crossover fighting game developed by Bandai Namco Studios and Sora Ltd., 
             and published by Nintendo for the Nintendo Switch. It is the fifth installment in the Super Smash Bros. series, 
             succeeding Super Smash Bros. for Nintendo 3DS and Wii U. The game follows the series' traditional style of gameplay: 
             controlling one of various characters, players must use differing attacks to weaken their opponents and knock them out 
--- web/navbar.php
+++ web/navbar.php
@@ -2,14 +2,14 @@
     $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 ?>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
 	<ul class="navbar-nav">
 		<li class="nav-item <?php if ($file === 'homepage') echo 'active' ?>">
-			<a class="title" href="homepage.php">Smash Ultimate Homepage</a>
+			<a class="title nav-link" href="homepage.php">Smash Ultimate Homepage</a>
 		</li>
 
 		<li class="nav-item <?php if ($file === 'assignments') echo 'active' ?>">
-			<a class="title" href="assignments.php">Assignments Page</a>
+			<a class="title nav-link" href="assignments.php">Assignments Page</a>
 		</li>
 	<ul>
 </nav>
