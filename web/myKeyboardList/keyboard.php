916bb6ce0ba4168e433bf2608f1c596afdd6e5c9
TheWifflebrain <blackice150@gmail.com>
2019-05-27 17:27:09 -0600

    prove5

---
+++ web/myKeyboardList/keyboard.php
@@ -0,0 +1,40 @@
+<?php
+require('dbConnect.php');
+$db = get_db();
+
+$query = 'SELECT keyboard_id, switch, sizek, typek, forsale, descriptionk, 
+photo, keyboard_name, user_id FROM keyboard';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+foreach ($keyboards as $keyboard)
+{
+    $id = $keyboard['keyboard_id'];
+    $switch = $keyboard['switch'];
+    $size = $keyboard['sizeK'];
+    $type = $keyboard['typeK'];
+    $forsale = $keyboard['forsale'];
+    $desc = $keyboard['descriptionk'];
+    $photo = $keyboard['photo'];
+    $name = $keyboard['keyboard_name'];
+
+    echo "<li><p>$name - $type</a></p></li>";
+    
+}
+?>
+    
+</body>
+</html>
