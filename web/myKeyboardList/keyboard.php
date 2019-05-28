2217503a6fe5623ceca6c94c2645159404cf327d
TheWifflebrain <blackice150@gmail.com>
2019-05-27 18:56:16 -0600

    prove5.6

--- web/myKeyboardList/keyboard.php
+++ web/myKeyboardList/keyboard.php
@@ -14,12 +14,15 @@ $keyboards = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>Keyboard</title>
 </head>
 <body>
 <h1>New Keyboards</h1>
+
 <?php
-foreach ($keyboards as $keyboard)
+foreach ($db->query('SELECT * FROM "keyboard"') as $keyboard)
 {
     $id = $keyboard['keyboard_id'];
     $switch = $keyboard['switch'];
@@ -29,18 +32,43 @@ foreach ($keyboards as $keyboard)
     $desc = $keyboard['descriptionK'];
     $photo = $keyboard['photo'];
     $name = $keyboard['keyboard_name'];
-
-    echo "<li><p>$name - $photo</a></p></li>";
-    
+?>
+<div class="border keyList">
+<div class="jumbotron text-center">
+    <h1><?php echo $keyboard["keyboard_name"]; ?></h1>
+        </div>
+        
+        <div>
+        <img id="sheikImage" class="img-thumbnail" src=<?php echo $keyboard["photo"]; ?>>
+        </div>
+        
+        <div>
+         <h3><?php echo $keyboard["descriptionK"]; ?></h3> 
+        </div>
+          
+        <div class="container">           
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Switch</th>
+                <th>Type</th>
+                <th>Size</th>
+                <th>For Sale</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><?php echo $keyboard["switch"]; ?></td>
+                <td><?php echo $keyboard["typeK"]; ?></td>
+                <td><?php echo $keyboard["sizeK"]; ?></td>
+                <td><?php echo $keyboard["forsale"]; ?></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    </div>
+<?php
 }
-
-foreach ($db->query('SELECT * FROM "keyboard"') as $row)
-            {
-              echo "<tr>";
-              echo "<td>" . $row["keyboard_id"];
-              echo "<td>" . $row["switch"];
-              echo "<td>" . $row["sizeK"];
-            }
 ?>
 
 
