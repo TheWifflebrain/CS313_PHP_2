725ea6837e599429002b70567b3b7af8050ecd72
TheWifflebrain <blackice150@gmail.com>
2019-06-01 20:08:13 -0600

    prove6.2

--- web/myKeyboardList/homepage.php
+++ web/myKeyboardList/homepage.php
@@ -1,11 +1,19 @@
 <?php
+
+if (!isset($_GET['username']))
+{
+	die("Error, username not specified...");
+}
+$username = htmlspecialchars($_GET['username']);
+
+
 require('dbConnect.php');
 $db = get_db();
-
-$query = 'SELECT keyboard_id, switch, sizeK, typeK, forsale, descriptionK, photo, keyboard_name, username FROM keyboard';
-$stmt = $db->prepare($query);
+$stmt = $db->prepare('SELECT k.*, p.* FROM keyboard k LEFT JOIN person p on k.username = p.username WHERE k.username=:user');
+$stmt->bindValue(':user', $username, PDO::PARAM_INT);
 $stmt->execute();
-$keyboards = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$username_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$username_code = $username_rows[0]['username'];
 ?>
 
 <!DOCTYPE html>
@@ -16,28 +24,26 @@ $keyboards = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Keyboard</title>
+    <title>Homepage</title>
 </head>
 <body>
-<h1 class="jumbotron text-center jumbo">New Keyboards</h1>
+<h1 class="jumbotron text-center jumbo"></h1>
 
 <?php
-foreach ($db->query('SELECT * FROM "keyboard"') as $row)
+foreach ($username_rows as $username_row)
 {
-    $id = $row['keyboard_id'];
-    $switch = $row['switch'];
-    $size = $row['sizek'];
-    $type = $row['typek'];
-    $forsale = $row['forsale'];
-    $desc = $row['descriptionk'];
-    $photo = $row['photo'];
-    $name = $row['keyboard_name'];
-    $user = $row['username'];
+    $switch = $username_row['switch'];
+    $size = $username_row['sizek'];
+    $type = $username_row['typek'];
+    $forsale = $username_row['forsale'];
+    $desc = $username_row['descriptionk'];
+    $photo = $username_row['photo'];
+    $name = $username_row['keyboard_name'];
+    $user = $username_row['username'];
 ?>
 <div class="border keyList">
     <div class="jumbotron text-center">
-        <h1><a href="keyboardPage.php?keyboard_id=<?php echo $id ?>"><?php echo $name; ?></a></h1>
-        <h4><a href="homepage.php?username=<?php echo $user ?>"><?php echo $user; ?></a></h4>
+        <h1><?php echo $user; ?></h1>
     </div>
         
         <div>
@@ -70,10 +76,9 @@ foreach ($db->query('SELECT * FROM "keyboard"') as $row)
         </div>
     </div>
 <?php
+break;
 }
 ?>
-
-
     
 </body>
 </html>
