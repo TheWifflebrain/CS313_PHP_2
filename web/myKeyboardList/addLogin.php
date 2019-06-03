9f2423a1a1f19cae3d205aa1a048a8142bef7acd
TheWifflebrain <blackice150@gmail.com>
2019-06-02 20:35:15 -0600

    prove6.35

---
+++ web/myKeyboardList/addLogin.php
@@ -0,0 +1,42 @@
+<?php
+
+if(isset($_POST['login'])){
+    session_start();
+    require "dbConnect.php";
+    $db = get_db();
+
+    $username = $_POST['username'];
+    $password = $_POST['pwd'];
+
+    if(empty($username) || empty($password)){
+        header("Location: login.php?error=incorrectUsernameOrPassword");
+        exit();
+    }
+    else{
+        try{
+            $statement = $db->prepare("SELECT passwordU FROM person WHERE username ='$username'");
+            $statement->execute();
+
+            $count=0;
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+                $checker=$row['passwordU'];
+                $pass=password_verify($password, $checker);
+                $count++;
+            }
+            if($pass){
+                session_start();
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $username;
+            }
+        }
+        catch(Exception $ex)
+        {
+            echo "Error with DB. Details: $ex";
+            die();
+        }
+        header("Location: homepage.php?username=$username");
+        die(); 
+    }   
+}
+?>
