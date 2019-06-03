39d69b4718ce922ed3ad419c6dc9506a576e669e
TheWifflebrain <blackice150@gmail.com>
2019-06-02 22:29:49 -0600

    prove6.57

--- web/myKeyboardList/addKeyboard.php
+++ web/myKeyboardList/addKeyboard.php
@@ -4,22 +4,22 @@
 
     $switch = htmlspecialchars($_POST['switch']);
     $size = htmlspecialchars($_POST['size']);
-    $type = htmlspecialchars($_POST['type']);
+    $typeK = htmlspecialchars($_POST['type']);
     $forsale = htmlspecialchars($_POST['forsale']);
-    $desc = htmlspecialchars($_POST['desc']);
+    $descK = htmlspecialchars($_POST['desc']);
     $photo = htmlspecialchars($_POST['photo']);
     $name = htmlspecialchars($_POST['keyboardName']);
     $user = htmlspecialchars($_POST['username']);
 
     $stmt = $db->prepare('INSERT INTO keyboard(switch, sizeK, typeK, 
     forsale, descriptionK, photo, keyboard_name, username_K) VALUES (:switch, 
-    size, type, forsale, desc, photo, keyboardName, user);');
+    size, typeK, forsale, descK, photo, keyboardName, user);');
 
     $stmt->bindValue(':switch', $switch, PDO::PARAM_STR);
     $stmt->bindValue(':size', $size, PDO::PARAM_STR);
-    $stmt->bindValue(':type', $type, PDO::PARAM_STR);
+    $stmt->bindValue(':type', $typeK, PDO::PARAM_STR);
     $stmt->bindValue(':forsale', $forsale, PDO::PARAM_STR);
-    $stmt->bindValue(':desc', $desc, PDO::PARAM_STR);
+    $stmt->bindValue(':desc', $descK, PDO::PARAM_STR);
     $stmt->bindValue(':phot', $phot, PDO::PARAM_STR);
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->bindValue(':user', $user, PDO::PARAM_STR);
