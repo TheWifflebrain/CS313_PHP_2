e779b42f34d861d650eb70f7867f57211d70ce16
TheWifflebrain <blackice150@gmail.com>
2019-06-02 19:38:12 -0600

    prove6.22

--- web/myKeyboardList/signUp.php
+++ web/myKeyboardList/signUp.php
@@ -11,7 +11,7 @@
 <body>
 <div class="container">
         <div class="row centered-form">
-        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        <div class="col-xs-12 col-sm-8 col-md-4">
         	<div class="panel panel-default">
         		<div class="panel-heading">
 			    		<h3 class="panel-title">Sign Up!</h3>
