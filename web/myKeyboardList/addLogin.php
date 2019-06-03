a39315b1ccc5cf0ee09655812e4edc48b56b9749
TheWifflebrain <blackice150@gmail.com>
2019-06-02 21:34:29 -0600

    prove6.47

--- web/myKeyboardList/addLogin.php
+++ web/myKeyboardList/addLogin.php
@@ -14,21 +14,49 @@ if(isset($_POST['login'])){
     }
     else{
         try{
-            $statement = $db->prepare("SELECT passwordU FROM person WHERE username ='$username'");
-            $statement->execute();
+            //$statement = $db->prepare("SELECT passwordU FROM person WHERE username ='$username'");
+            //$statement->execute();
 
-            $count=0;
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-            {
-                $checker=$row['passwordU'];
+            //$count=0;
+            //while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            //{
+              //  $checker=$row['passwordU'];
                 //$password=password_verify($password, $checker);
                 //count++;
-                $hashed_password=password_hash($password, PASSWORD_DEFAULT);
-                if($hashed_password == $checker){
-                    session_start();
-                    $_SESSION['loggedin'] = true;
-                    $_SESSION['username'] = $username;
-                }
+               // $hashed_password=password_hash($password, PASSWORD_DEFAULT);
+               // if($hashed_password == $checker){
+               //     session_start();
+              //      $_SESSION['loggedin'] = true;
+              //      $_SESSION['username'] = $username;
+               // }
+               $sql = "SELECT * FROM person WHERE username=?;";
+               $stmt = mysql_stmt_init($conn);
+               if(!mysql_stmt_prepare($stmt, $sql)){
+                header("Location: login.php?incorrectUsernameOrPassword");
+                exit();
+               }
+               else{
+                   mysql_stmt_bind_param($stmt, "s",$username);
+                   mysql_stmt_execute($stmt);
+                   $result = mysql_stmt_get_result($stmt);
+                   if($row = mysql_fetch_assoc($result)){
+                       $pwdCheck = password_verify($password, $row['passwordU']);
+                       if($pwdCheck==false){
+                        header("Location: login.php?incorrectUsernameOrPassword");
+                        exit();
+                       }
+                       else if($pwdCheck ==true){
+                           session_start();
+                           $_SESSION['username'] = $row['username'];
+                           header("Location: homepage.php?username=$username");
+                           exit();
+                       }
+                       else{
+                        header("Location: login.php?incorrectUsernameOrPassword");
+                        exit();
+                       }
+                   } 
+               }
             }
             
         }
@@ -36,10 +64,10 @@ if(isset($_POST['login'])){
         {
             echo "Error with DB. Details: $ex";
             header("Location: login.php?incorrectUsernameOrPassword");
-            die();
+            exit();
         } 
         header("Location: homepage.php?username=$username");
-        die();
+        exit();
     }   
 }
 ?>
