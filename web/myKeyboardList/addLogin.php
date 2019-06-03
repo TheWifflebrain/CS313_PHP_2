2b55a22c20fed884fb029af17056e303cd6f1ac9
TheWifflebrain <blackice150@gmail.com>
2019-06-02 21:37:34 -0600

    prove6.49

--- web/myKeyboardList/addLogin.php
+++ web/myKeyboardList/addLogin.php
@@ -22,7 +22,7 @@ if(isset($_POST['login'])){
             {
                 $checker=$row['passwordU'];
                 $password=password_verify($password, $checker);
-                count++;
+                $count++;
             }
             if($pass){
                 session_start();
