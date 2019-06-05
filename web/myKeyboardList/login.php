9aa50c212bf80f80752ca3de2d29d1fe405e9591
TheWifflebrain <blackice150@gmail.com>
2019-06-04 23:35:22 -0600

    fixing log in6

--- web/myKeyboardList/login.php
+++ web/myKeyboardList/login.php
@@ -1,3 +1,47 @@
+<?php
+	session_start();
+	$badLogin = false;
+
+	if (isset($_POST['username']) && isset($_POST['pwd']))
+	{
+		// they have submitted a username and password for us to check
+		$username = $_POST['username'];
+		$password = $_POST['pwd'];
+		// Connect to the DB
+		require("dbConnect.php");
+		$db = get_db();
+		$query = 'SELECT passwordU FROM person WHERE username=:username';
+		$statement = $db->prepare($query);
+		$statement->bindValue(':username', $username);
+		$result = $statement->execute();
+		if ($result)
+		{
+			$row = $statement->fetch();
+			$hashedPasswordFromDB = $row['passwordU'];
+			// now check to see if the hashed password matches
+			if (password_verify($password, $hashedPasswordFromDB))
+			{
+				// password was correct, put the user on the session, and redirect to home
+				$_SESSION['username'] = $username;
+				header("Location: listKeyboard.php");
+				die(); // we always include a die after redirects.
+			}
+			else
+			{
+                $badLogin = true;
+                header("Location: login.php?invalidcredentials");
+                die();
+			}
+		}
+		else
+		{
+            $badLogin = true;
+            header("Location: login.php?invalidcredentials");
+            die();
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +53,14 @@
     <title>Login</title>
 </head>
 <body>
+<?php
+
+if ($badLogin)
+{
+	echo "Incorrect username or password!<br /><br />\n";
+}
+?>
+
 <div class="container">
         <div class="row centered-form">
         <div class="col-xs-12 col-sm-8 col-lg-12 centered-form">
@@ -28,6 +80,7 @@
 							</div>
 			    			
 			    			<input type="submit" name="login" value="login" class="btn btn-info btn-block">
+
 			    		
 			    		</form>
 			    		</div>
