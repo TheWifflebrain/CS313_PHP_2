c66327fcca9726cd3d43b20cd278bee2f070f66a
TheWifflebrain <blackice150@gmail.com>
2019-06-08 16:01:14 -0600

    deleteKeyboard10

--- web/myKeyboardList/removeKeyboard.php
+++ web/myKeyboardList/removeKeyboard.php
@@ -1,8 +1,6 @@
 <?php
-    //$keyID = $_GET['keyID'];
     $keyID = $_GET['keyboard_id'];
-    echo $keyID;
-    $stmt = $db->prepare("DELETE FROM keyboard WHERE keyboard_id=$keyID;");
+    $stmt = $db->prepare('DELETE FROM keyboard WHERE keyboard_id=$keyID;');
     $stmt->execute();
     header("Location: frontpage.php");
     die();
