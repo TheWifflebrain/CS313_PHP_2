07667c86668c3232e0c166a4589b56a019028d74
TheWifflebrain <blackice150@gmail.com>
2019-06-08 12:03:15 -0600

    editKeyboard19

--- web/myKeyboardList/editKeyboard.php
+++ web/myKeyboardList/editKeyboard.php
@@ -31,31 +31,32 @@ $keyID = htmlspecialchars($_GET['keyboard_id']);
 			 			<div class="panel-body">
 			    		<form method="POST" action="editKeyboardHandler.php?keyboard_id=<?php echo $keyID ?>">
                             <div class="form-group">
-                                    <input type="text" name="keyboardName" id="keyboardName" class="form-control input-sm" placeholder="Keyboard Name" required>
+                                    <input type="text" name="keyboardName" id="keyboardName" class="form-control input-sm" placeholder="Keyboard Name">
                             </div>
                             <div class="form-group">
-                                    <input type="text" name="desc" id="desc" class="form-control input-sm" placeholder="Description" required>
+                                    <input type="text" name="desc" id="desc" class="form-control input-sm" placeholder="Description">
                             </div>
                             <div class="form-group">
-                                    <input type="text" name="switch" id="switch" class="form-control input-sm" placeholder="Switch" required>
+                                    <input type="text" name="switch" id="switch" class="form-control input-sm" placeholder="Switch">
                             </div>
                             <div class="form-group">
-                                    <input type="text" name="size" id="size" class="form-control input-sm" placeholder="Size" required>
+                                    <input type="text" name="size" id="size" class="form-control input-sm" placeholder="Size">
                             </div>
                             <div class="form-group">
-                                    <input type="text" name="type" id="type" class="form-control input-sm" placeholder="Type" required>
+                                    <input type="text" name="type" id="type" class="form-control input-sm" placeholder="Type">
                             </div>
                             <div class="form-group">
-                                    <input type="text" name="forsale" id="forsale" class="form-control input-sm" placeholder="For Sale" required>
+                                    <input type="text" name="forsale" id="forsale" class="form-control input-sm" placeholder="For Sale">
                             </div>
                             <div class="form-group">
-                                    <input type="text" name="photo" id="photo" class="form-control input-sm" placeholder="Photo URL" required>
+                                    <input type="text" name="photo" id="photo" class="form-control input-sm" placeholder="Photo URL">
                             </div>
                             <input type="hidden" name="username_id" value="<?php echo $usernameS?>">
                             <input type="hidden" name="keyboard_id" value="<?php echo $keyID?>">
                             <input type="submit" name="Add" value="Add" class="btn btn-info btn-block">
 			    		</form>
 			    		</div>
+                        <p class="panel-title">If you leave fields blank then it will keep your previous entry.</p>
 	    			</div>
     		</div>
     	</div>
