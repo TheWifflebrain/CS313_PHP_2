e7444735021a0b1b73843dd8c4dab01828744cdc
TheWifflebrain <blackice150@gmail.com>
2019-06-08 15:50:02 -0600

    deleteKeyboard5

--- web/myKeyboardList/removeKeyboard.php
+++ web/myKeyboardList/removeKeyboard.php
@@ -1,5 +1,6 @@
 <?php
     $keyID = $_GET['keyID'];
+    $keyID = $_GET['keyboardid'];
 
     $stmt = $db->prepare('DELETE FROM keyboard WHERE keyboard_id=:id;');
     $stmt->bindValue(':id', $keyID, PDO::PARAM_INT);
