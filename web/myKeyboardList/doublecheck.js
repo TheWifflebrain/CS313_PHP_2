e1108d23fdbd65ad3be87057599ebec7e3478042
TheWifflebrain <blackice150@gmail.com>
2019-06-08 18:18:36 -0600

    deleteComment10

--- web/myKeyboardList/doublecheck.js
+++ web/myKeyboardList/doublecheck.js
@@ -9,6 +9,6 @@ function doublecheck2(){
     var keyID = document.getElementById("keyboard_id").value;
     var commentID = document.getElementById("comment_id").value;
     if(confirm("Are you sure you want to delete this comment?")){
-        window.location.href = "removeCommentHandler.php?keyboard_id=" + keyID +"?comment_id=" +commentID;
+        window.location.href = "removeCommentHandler.php?keyboard_id=" + keyID +"&comment_id=" +commentID;
     }
 }
