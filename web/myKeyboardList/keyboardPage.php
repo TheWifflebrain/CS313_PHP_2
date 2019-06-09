99b87f7290883dec336e831c0249f6507377e81a
TheWifflebrain <blackice150@gmail.com>
2019-06-08 19:25:45 -0600

    editComment3

--- web/myKeyboardList/keyboardPage.php
+++ web/myKeyboardList/keyboardPage.php
@@ -131,7 +131,7 @@ if($check == 1){
                   <td><input onclick="showInput()" type="submit" name="Edit" value="Edit" class="btn btn-info btn-block"></a></td>
                   <td><input onclick="doublecheck2()" type="button" name="Remove" value="Remove" class="btn btn-info btn-block"></td>
                   <tr>
-                    <div id="edits">
+                    <div id="edits" display="none">
                       <td><input type="text" name="editC" id="editC" class="form-control input-sm" placeholder="Edit Comment" required></td>
                       <td><input onclick="editComment()" type="button" name="Submit" value="Submit" class="btn btn-info btn-block"></td>   
                     </div>
