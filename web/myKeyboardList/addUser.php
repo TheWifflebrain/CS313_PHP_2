3b8d74e609832e45466ba75f3a8b0ce279378819
TheWifflebrain <blackice150@gmail.com>
2019-06-08 23:17:03 -0600

    usertaken31

--- web/myKeyboardList/addUser.php
+++ web/myKeyboardList/addUser.php
@@ -15,6 +15,7 @@
         $query0 = "SELECT person.username FROM person WHERE username='$username'";
         $statement0 =$db->prepare($query0);
         $result0 = $statement0->execute();
+        echo $result0;
         if(!$result0)
         {
             header("Location: signUp.php?taken=1");
