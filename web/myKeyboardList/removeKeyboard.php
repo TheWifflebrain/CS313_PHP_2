5b8a7202c3aff56f83b088886fd3bca1f1f10be1
TheWifflebrain <blackice150@gmail.com>
2019-06-12 17:24:11 -0600

    betterSQLstatements6

--- web/myKeyboardList/removeKeyboard.php
+++ web/myKeyboardList/removeKeyboard.php
@@ -1,8 +1,8 @@
 <?php
-    $keyID = $_GET['keyboard_id'];
+    $keyboardID = $_GET['keyboard_id'];
     require('dbConnect.php');
     $db = get_db();
-    $stmt = $db->prepare("DELETE FROM keyboard WHERE keyboard_id=$keyID;");
+    $stmt = $db->prepare("DELETE FROM keyboard WHERE keyboard_id=$keyboardID;");
     $stmt->execute();
     header("Location: frontpage.php");
     die();
