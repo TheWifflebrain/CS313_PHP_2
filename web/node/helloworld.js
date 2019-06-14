8c525cf5c8db968ea1f17ab30e4eda96ab55365e
TheWifflebrain <blackice150@gmail.com>
2019-06-13 19:50:37 -0600

    helloworld node js2

--- web/node/helloworld.js
+++ web/node/helloworld.js
@@ -23,11 +23,24 @@ http.createServer(function (req, res)
         res.write(JSON.stringify(myData));
         res.end();
     }
+    else if(req.url.includes("/mult"))
+    {
+        //ex. /mult?num1=45&num2=5
+        var url = require('url');
+        var numbers = url.parse(req.url, true);
+        var qNumbers = numbers.query;
+        var num1 = qNumbers.num1;
+        var num2 = qNumbers.num2;
+        var result = num1 * num2;
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write(num1 + "*"+ num2 + " = " + result);
+        res.end();
+    }
     else
     {
         res.writeHead(404, {"Content-Type": "text/html"});
         res.write('<html><head></head><body>');
-        res.write('<h1>Page Not Found</h1>');
+        res.write('<h1>404 Page Not Found</h1>');
         res.write('</body></html>');
         res.end();
     }
