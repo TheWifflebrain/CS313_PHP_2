fa6304ab6de187f897ef525bbf551e030f54299d
TheWifflebrain <blackice150@gmail.com>
2019-06-13 21:36:28 -0600

    helloworld node js3

--- web/node/helloworld.js
+++ web/node/helloworld.js
@@ -23,6 +23,16 @@ http.createServer(function (req, res)
         res.write(JSON.stringify(myData));
         res.end();
     }
+    else if(req.url == "/prettyGetData")
+    {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        Array.from(Object.keys(myData)).forEach(function(key){
+            res.write(key + ": ")
+            res.write((myData[key]));
+            res.write("\n");
+        });
+        res.end();
+    }
     else if(req.url.includes("/mult"))
     {
         //ex. /mult?num1=45&num2=5
@@ -34,6 +44,31 @@ http.createServer(function (req, res)
         var result = num1 * num2;
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.write(num1 + "*"+ num2 + " = " + result);
+        res.end();
+    }
+    else if(req.url.includes("/div"))
+    {
+        //ex. /div?num1=45&num2=5
+        var url = require('url');
+        var numbers = url.parse(req.url, true);
+        var qNumbers = numbers.query;
+        var num1 = qNumbers.num1;
+        var num2 = qNumbers.num2;
+        var result = num1 / num2;
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write(num1 + "/" + num2 + " = " + result);
+        res.end();
+    }
+    else if(req.url.includes("/printthis"))
+    {
+        //ex. /printthis?print=how%20are%20you
+        //%20 add spaces
+        var url = require('url');
+        var text = url.parse(req.url, true);
+        var qtext = text.query;
+        var print = qtext.print;
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write(print);
         res.end();
     }
     else
